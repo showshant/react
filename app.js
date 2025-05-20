9100ed7db89d8b97ee464cbaf2ba8d94a2e3de43
showshant <showshant716@gmail.com>
2025-05-20 23:40:33 +0545

    third commit

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
-import React from "react";
+import React, { createContext } from "react";
 import ReactDOM from "react-dom/client";
 
-const title = (
-  <h1 className="head" tabIndex="5">
-    {2 + 2 + 6}
-    <br></br>
-    Hello guys k cha hallichalli
-  </h1>
-);
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *    - RestaurantCard
+ *      - Img
+ *      - Name of Restaurant, Star Rating, cuisine, delivery time
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
 
-const number = 500;
-const HeadingComponent = () => (
-  <div id="container">
-    {console.log("oi mula")}
-    <br></br>
-    {2 + 2}
-    {title}
-    {number}
-    <h1 className="heading">
-      <br></br>
-      {2 + 2 + 3} <br></br> Hello guys
-    </h1>
-  </div>
-);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent />);
